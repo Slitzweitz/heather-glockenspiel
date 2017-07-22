5c015f2ab121d71670bddb4bf635503a8e45111c
System <none>
2017-07-22 05:15:33 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -14,82 +14,36 @@
 var express = require('express');
 var mongodb = require('mongodb');
 var app = express();
-var dbSongs="";
+
+const GoogleImages = require('google-images');
+ 
+const client = new GoogleImages('CSE ID', 'API KEY');
+ 
+var newSearch = client.search(req.params[0])
+    .then(images => {
+        /*
+        [{
+            "url": "http://steveangello.com/boss.jpg",
+            "type": "image/jpeg",
+            "width": 1024,
+            "height": 768,
+            "size": 102451,
+            "thumbnail": {
+                "url": "http://steveangello.com/thumbnail.jpg",
+                "width": 512,
+                "height": 512
+            }
+        }]
+         */
+    });
+ 
+// paginate results 
+client.search('Steve Angello', {page: 2});
 
 app.use(express.static('public'));
 
-
-// Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname, details set in .env
-var uri = 'mongodb://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST+':'+process.env.PORT+'/'+process.env.DB;
-
-mongodb.MongoClient.connect(uri, function(err, db) {
-  if(err) throw err;
-  dbSongs+="<h1>MongoDB Example</h1>";
-  dbSongs+="Connecting to db "+process.env.DB+"<br />";
-  
-  /*
-   * First we'll add a few songs. Nothing is required to create the 
-   * songs collection; it is created automatically when we insert.
-   */
-
-  var songs = db.collection('songs');
-  dbSongs+="Creating collection 'songs'<br />";
-  
-   // Note that the insert method can take either an array or a dict.
-
-  songs.insert(seedData, function(err, result) {
-    
-    if(err) throw err;
-
-    /*
-     * Then we need to give Boyz II Men credit for their contribution
-     * to the hit "One Sweet Day".
-     */
-
-    songs.update(
-      { song: 'One Sweet Day' }, 
-      { $set: { artist: 'Mariah Carey ft. Boyz II Men' } },
-      function (err, result) {
-        
-        if(err) throw err;
-
-        /*
-         * Finally we run a query which returns all the hits that spend 10 or
-         * more weeks at number 1.
-         */
-
-        songs.find({ weeksAtOne : { $gte: 10 } }).sort({ decade: 1 }).toArray(function (err, docs) {
-
-          if(err) throw err;
-
-          docs.forEach(function (doc) {
-            console.log(
-              'In the ' + doc['decade'] + ', ' + doc['song'] + ' by ' + doc['artist'] + 
-              ' topped the charts for ' + doc['weeksAtOne'] + ' straight weeks.'
-            );
-            dbSongs+="Adding "+doc['artist']+" - "+doc['song']+" into 'songs'<br />";
-          });
-         
-          // Since this is an example, we'll clean up after ourselves.
-          songs.drop(function (err) {
-            dbSongs+="Dropping collection 'songs'<br />";
-            if(err) throw err;
-          
-            // Only close the connection when your app is terminating.
-            db.close(function (err) {
-              dbSongs+="Closing db " + process.env.DB;
-              dbSongs+="<script src=\"https://button.glitch.me/button.js\" data-style=\"glitch\"></script><div class=\"glitchButton\" style=\"position:fixed;top:20px;right:20px;\"></div>";
-              if(err) throw err;
-            });
-          });
-        });
-      }
-    );
-  });
-});
-
-app.get("/", function (request, response) {
-  response.send(dbSongs);
+app.get("/", function (req, res) {
+  res.send();
 });
 
 // listen for requests :)
